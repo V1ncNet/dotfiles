ee91839cbd229b29906760f0d26c40961bf0a91a
Vincent Nadoll <vincent.nadoll@googlemail.com>
2024-08-25 19:50:26 +0200

    Add nix-darwin and Home Manager

--- flake.nix
+++ flake.nix
@@ -3,8 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs@{ self, nixpkgs, nix-darwin, home-manager }:
+  {
   };
 }
