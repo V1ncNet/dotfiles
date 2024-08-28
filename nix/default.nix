8909f8b7c5a195e5da25cf77fba82ddfdc5e120a
Vincent Nadoll <vincent.nadoll@googlemail.com>
2024-08-28 20:39:22 +0200

    Add hosts ceres and sagittarius

--- nix/default.nix
+++ nix/default.nix
@@ -16,4 +16,36 @@
       ./base.nix
     ];
   };
+
+  "vincent@ceres" = home-manager.lib.homeManagerConfiguration {
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    extraSpecialArgs = { inherit inputs; };
+
+    modules = [
+      {
+        home = {
+          username = "vincent";
+          homeDirectory = "/home/vincent";
+          stateVersion = "24.05";
+        };
+      }
+      ./base.nix
+    ];
+  };
+
+  "vincent@sagittarius" = home-manager.lib.homeManagerConfiguration {
+    pkgs = nixpkgs.legacyPackages.aarch64-linux;
+    extraSpecialArgs = { inherit inputs; };
+
+    modules = [
+      {
+        home = {
+          username = "vincent";
+          homeDirectory = "/home/vincent";
+          stateVersion = "24.05";
+        };
+      }
+      ./base.nix
+    ];
+  };
 }
